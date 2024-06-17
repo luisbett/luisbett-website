cc2611c9eb3a32de74da1a69411a33549a0955ce
Luis Fellipy Bett <luisfbett@gmail.com>
2024-06-17 13:54:55 +0100

    add new technologies to skills carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -7,6 +7,8 @@
         <input type="radio" name="elements" id="element3">
         <input type="radio" name="elements" id="element4">
         <input type="radio" name="elements" id="element5">
+        <input type="radio" name="elements" id="element6">
+        <input type="radio" name="elements" id="element7">
                 
         <div class="carousel-item" id="one">
 
@@ -14,7 +16,7 @@
             <IconBrandHtml5 color="#26AF70" :size="256" stroke-width="2"/>
             <h3>{{ $t( 'carouselExpTitle' ) }}: 3+ {{ $t( 'carouselExpTime' ) }}</h3>
 
-            <label for="element5" class="previous"><IconChevronLeft class="left-arrow" color="white" :size="50" stroke-width="3"/></label>
+            <label for="element7" class="previous"><IconChevronLeft class="left-arrow" color="white" :size="50" stroke-width="3"/></label>
             <label for="element2" class="next"><IconChevronRight class="right-arrow" color="white" :size="50" stroke-width="3"/></label>
 
         </div>
@@ -34,7 +36,7 @@
 
             <h2>JavaScript</h2>
             <IconBrandJavascript color="#26AF70" :size="256" stroke-width="2"/>
-            <h3>{{ $t( 'carouselExpTitle' ) }}: 2+ {{ $t( 'carouselExpTime' ) }}</h3>
+            <h3>{{ $t( 'carouselExpTitle' ) }}: 3+ {{ $t( 'carouselExpTime' ) }}</h3>
 
             <label for="element2" class="previous"><IconChevronLeft class="left-arrow" color="white" :size="50" stroke-width="3"/></label>
             <label for="element4" class="next"><IconChevronRight class="right-arrow" color="white" :size="50" stroke-width="3"/></label>
@@ -54,11 +56,33 @@
 
         <div class="carousel-item" id="five">
 
+            <h2>React</h2>
+            <IconBrandReact color="#26AF70" :size="256" stroke-width="2"/>
+            <h3>{{ $t( 'carouselExpTitle' ) }}: 1+ {{ $t( 'carouselExpTime' ) }}</h3>
+
+            <label for="element4" class="previous"><IconChevronLeft class="left-arrow" color="white" :size="50" stroke-width="3"/></label>
+            <label for="element6" class="next"><IconChevronRight class="right-arrow" color="white" :size="50" stroke-width="3"/></label>
+
+        </div>
+
+        <div class="carousel-item" id="six">
+
+            <h2>Node</h2>
+            <IconBrandNodejs color="#26AF70" :size="256" stroke-width="2"/>
+            <h3>{{ $t( 'carouselExpTitle' ) }}: 1+ {{ $t( 'carouselExpTime' ) }}</h3>
+
+            <label for="element5" class="previous"><IconChevronLeft class="left-arrow" color="white" :size="50" stroke-width="3"/></label>
+            <label for="element7" class="next"><IconChevronRight class="right-arrow" color="white" :size="50" stroke-width="3"/></label>
+
+        </div>
+
+        <div class="carousel-item" id="seven">
+
             <h2>SQL</h2>
             <IconDatabase color="#26AF70" :size="256" stroke-width="2"/>
             <h3>{{ $t( 'carouselExpTitle' ) }}: 5+ {{ $t( 'carouselExpTime' ) }}</h3>
 
-            <label for="element4" class="previous"><IconChevronLeft class="left-arrow" color="white" :size="50" stroke-width="3"/></label>
+            <label for="element6" class="previous"><IconChevronLeft class="left-arrow" color="white" :size="50" stroke-width="3"/></label>
             <label for="element1" class="next"><IconChevronRight class="right-arrow" color="white" :size="50" stroke-width="3"/></label>
 
         </div>
@@ -70,6 +94,8 @@
             <label class="dots" id="dot3" for="element3"></label>
             <label class="dots" id="dot4" for="element4"></label>
             <label class="dots" id="dot5" for="element5"></label>
+            <label class="dots" id="dot6" for="element6"></label>
+            <label class="dots" id="dot7" for="element7"></label>
 
         </div>
 
@@ -83,6 +109,8 @@
     import { IconBrandCss3 } from '@tabler/icons-vue'
     import { IconBrandJavascript } from '@tabler/icons-vue'
     import { IconBrandVue } from '@tabler/icons-vue'
+    import { IconBrandReact } from '@tabler/icons-vue'
+    import { IconBrandNodejs } from '@tabler/icons-vue'
     import { IconDatabase } from '@tabler/icons-vue'
     import { IconChevronLeft } from '@tabler/icons-vue'
     import { IconChevronRight } from '@tabler/icons-vue'
@@ -94,6 +122,8 @@
             IconBrandCss3,
             IconBrandJavascript,
             IconBrandVue,
+            IconBrandReact,
+            IconBrandNodejs,
             IconDatabase,
             IconChevronLeft,
             IconChevronRight
@@ -145,7 +175,7 @@
         right: 3px;
     }
 
-    #element1, #element2, #element3, #element4, #element5 {
+    #element1, #element2, #element3, #element4, #element5, #element6, #element7 {
         display: none;
     }
 
@@ -186,7 +216,9 @@
     #element2:checked ~ #two,
     #element3:checked ~ #three,
     #element4:checked ~ #four,
-    #element5:checked ~ #five {
+    #element5:checked ~ #five,
+    #element6:checked ~ #six,
+    #element7:checked ~ #seven {
         z-index: 9;
     }
 
@@ -194,7 +226,9 @@
     #element2:checked ~ .nav #dot2,
     #element3:checked ~ .nav #dot3,
     #element4:checked ~ .nav #dot4,
-    #element5:checked ~ .nav #dot5 {
+    #element5:checked ~ .nav #dot5,
+    #element6:checked ~ .nav #dot6,
+    #element7:checked ~ .nav #dot7 {
         background-color: #FFFFFF;
     }
 
